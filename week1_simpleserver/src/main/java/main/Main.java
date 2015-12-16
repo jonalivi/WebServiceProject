4c3b29d445cc8cdc49e5ca3d3a5ca6a0e6fa76a1
Nadya Polidorova <moooncancer@gmail.com>
2015-12-16 19:19:27 +0300

    Entry point - server starts at localhost:8080, requests to "/mirror" are handled by GetParamServlet

--- week1_simpleserver/src/main/java/main/Main.java
+++ week1_simpleserver/src/main/java/main/Main.java
@@ -1,7 +1,26 @@
 package main;
 
+import org.eclipse.jetty.http.HttpParser;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.log.Log;
+import servlets.GetParamServlet;
+
 public class Main {
     public static void main(String[] args) {
-
+        GetParamServlet servlet = new GetParamServlet();
+        ServletContextHandler handler =
+                new ServletContextHandler(ServletContextHandler.SESSIONS);
+        handler.addServlet(new ServletHolder(servlet), "/mirror");
+        Server server = new Server(8080);
+        server.setHandler(handler);
+        try {
+            server.start();
+            Log.getLogger(Main.class).info("Server started");
+            server.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
