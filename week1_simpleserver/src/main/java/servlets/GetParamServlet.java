8363afbd5195aa20e00cc1df8cfafcf8ad02f391
Nadya Polidorova <moooncancer@gmail.com>
2015-12-16 18:48:35 +0300

    GetParamServlet implemented - prints out the value of the first GET parameter

--- week1_simpleserver/src/main/java/servlets/GetParamServlet.java
+++ week1_simpleserver/src/main/java/servlets/GetParamServlet.java
@@ -1,5 +1,24 @@
 package servlets;
 
-public class GetParamServlet {
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
 
+public class GetParamServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws IOException {
+        Map<String, String[]> params =
+                request.getParameterMap();
+        Iterator<String> it = params.keySet().iterator();
+        if (it.hasNext()) {
+            response.getWriter().println(Arrays.toString(params.get(it.next())));
+        }
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
 }
